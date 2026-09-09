aed4248cfc7607087397df476334c5d3af55f644
Ulysson Fontenele <ulysson52@gmail.com>
2026-09-08 22:11:39 -0300

    Como criar seu primeiro banco de dados com SQLite

---
+++ app.py
@@ -0,0 +1,11 @@
+#Importa a biblioteca sqlite3, ela permite que o Python trabalhe com bancos de dados SQLite - é um banco mais simples nativo, e muito funcional.
+import sqlite3
+
+#Cria uma conexão com o banco de dados, se o arquivo "banco.bd" não existir, o SQLIte ira cria-lo de forma automatica. 
+conexao = sqlite3.connect("banco.bd")
+
+#Exite a msg para mostrar que deu tudo certo rsrs!
+print("Banco conectado com sucesso!")
+
+#Fecha a conexão com o banco de dados, vai observar que muito dos nossos códigos vão sempre fechar, o SQLite em particular não tem um bom desempenho em muitos acessos simutâneos! 
+conexao.close()
