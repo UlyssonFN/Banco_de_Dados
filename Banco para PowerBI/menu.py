fb5ae87c3f37911dfe1230dfec618938697dfe87
Ulysson Fontenele <ulysson52@gmail.com>
2026-09-21 21:46:50 -0300

    Criado arquivo menu.py

---
+++ Banco para PowerBI/menu.py
@@ -0,0 +1,13 @@
+#menu
+import sqlite3
+
+conexao = sqlite3.connect("BDSYS.bd")
+
+cursor = conexao.cursor()
+
+user = input("Usuário: ")
+password = input("Senha: ")
+
+
+
+#Vou iniciar e vou terminar outro dia, porque a viagem hoje foi longa. 300km
