a61889a31a1e59690b0b513ae3f58c4df0948ed9
Ulysson Fontenele <ulysson52@gmail.com>
2026-09-23 20:53:02 -0300

    Criado validação de usuário e senha de acordo com o cadastro dentro do banco de dados na tabela usuários

--- Banco para PowerBI/menu.py
+++ Banco para PowerBI/menu.py
@@ -3,15 +3,32 @@ import sqlite3
 
 conexao = sqlite3.connect("BDSYS.bd")
 
-user = input("Usuário: ")
-password = input("Senha: ")
-
-
 cursor = conexao.cursor()
+try:
+    user = input("Usuário: ")
+    password = input("Senha: ")
 
-conexao.commit()
+    cursor.execute("""
+                SELECT NOME FROM USUARIOS WHERE NOME=?""",(user,))
 
-conexao.close()
+    (usuario,) = cursor.fetchone()
+
+    cursor.execute("""
+                SELECT SENHA FROM USUARIOS WHERE SENHA=?""",(password,))
+
+    (senha,) = cursor.fetchone()
+
+    if user == usuario and password == senha:
+        print("Bem vindo ao sistema")
+    
+    conexao.commit()
+    conexao.close()
+    
+except Exception:
+    print("Usuário ou Senha inválida")
+    
+    conexao.commit()
+    conexao.close()
 
 
 
